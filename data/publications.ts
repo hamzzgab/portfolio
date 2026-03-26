eef5bcb45e489341c8936e591d15a9aee2a4a08b
Hamza Gabajiwala <47095611+hamzzgab@users.noreply.github.com>
2026-03-26 17:22:15 +0000

    Update publications.ts

--- data/publications.ts
+++ data/publications.ts
@@ -8,6 +8,8 @@ export interface Publication {
 export const publications: Publication[] = [
   {
     title: "Comparative Analysis between InceptionResnetV2 and InceptionV3 for Attention Based Image Captioning",
+    venue: "IEEE",
+    year: "2021",
   },
   {
     title: "Comprehensive Review of Various Optimization Algorithms for Image Captioning",
